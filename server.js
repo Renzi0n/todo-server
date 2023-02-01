20c6c1c9469a5bbc93883f04daa50773d6ea208e
areshetov <areshetov@phoenixit.ru>
2023-02-02 01:31:17 +0300

    fix

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbJsonPath = path.join(__dirname, `../../tmp/db.json`);
+
+fs.appendFile(dbJsonPath, `{
+  "tasks": [
+    {
+      "id": 1,
+      "name": "Wash",
+      "info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      "isImportant": false,
+      "isCompleted": false
+    },
+    {
+      "id": 2,
+      "name": "123",
+      "info": "222",
+      "isImportant": true,
+      "isCompleted": false
+    },
+    {
+      "name": "Wash",
+      "info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      "isImportant": false,
+      "id": 3,
+      "isCompleted": false
+    }
+  ]
+}
+`, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("tmp/db.json");
+const router = jsonServer.router(dbJsonPath);
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
