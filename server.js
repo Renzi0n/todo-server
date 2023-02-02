e76c14000b342c06379c956798b34ef6e293478e
areshetov <areshetov@phoenixit.ru>
2023-02-02 19:16:50 +0300

    add serving static

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const path = require('path');
+const dbJsonPath = path.join(__dirname, "public", "db.json");
+const { static } = require('express');
+
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(dbJsonPath);
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
+
+server.use(static(path.join(__dirname, "public")));
 
 server.use(middlewares);
 
